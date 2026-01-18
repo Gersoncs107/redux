c04214c9c4e230b604f40df1681c8db321026cab
Gersoncs107 <gerson.silva107@gmail.com>
2026-01-17 21:36:50 -0300

    redux-update

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,17 @@
-import { StrictMode } from 'react'
+import { act, StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+
+const counterReduce = (state, action) => {
+  if (action.type === 'INCREMENT') {
+    return state + 1
+  }
+  if (action.type === 'DECREMENT') {
+    return state - 1
+  }
+  return state
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
