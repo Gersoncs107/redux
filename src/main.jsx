d6d1d0c10a60863d909c5d05b2217aa5b7c07254
Gersoncs107 <gerson.silva107@gmail.com>
2026-01-17 21:50:54 -0300

    redux-update

--- src/main.jsx
+++ src/main.jsx
@@ -10,12 +10,23 @@ const counterReducer = (state = 0, action) => {
       return state + 1
     case 'DECREMENT':
       return state - 1
-    case 'RESET':
+    case 'ZERO':
       return 0
     default:
       return state
   }
 }
+
+const store = createStore(counterReducer)
+
+console.log(store.getState())
+store.dispatch({type: 'INCREMENT'})
+store.dispatch({type: 'INCREMENT'})
+store.dispatch({type: 'INCREMENT'})
+console.log(store.getState())
+store.dispatch({type: 'ZERO'})
+store.dispatch({type: 'DECREMENT'})
+console.log(store.getState())
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
