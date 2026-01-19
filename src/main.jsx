646f2b3f261d0a961e326d644d0c8d0376d12ae4
Gersoncs107 <gerson.silva107@gmail.com>
2026-01-18 22:29:03 -0300

    redux-update

--- src/main.jsx
+++ src/main.jsx
@@ -13,19 +13,34 @@ const noteReducer = (state = [], action) => {
 
 const store = createStore(noteReducer)
 
+store.dispatch({
+  type: 'NEW_NOTE',
+  payload: {
+    content: 'the app state is in redux store',
+    important: true,
+    id: 1
+  }
+})
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  payload: {
+    content: 'state changes are made with actions',
+    important: false,
+    id: 2
+  }
+})
+
 const App = () => {
   return (
     <div>
-      <div>{store.getState()}</div>
-      <button onClick={() => store.dispatch({ type: 'INCREMENT' })}>
-        plus
-      </button>
-      <button onClick={() => store.dispatch({ type: 'DECREMENT' })}>
-        minus
-      </button>
-      <button onClick={() => store.dispatch({ type: 'ZERO' })}>
-        zero
-      </button>
+      <ul>
+        {store.getState().map(note => (
+          <li key={note.id}>
+            {note.content} <strong>{note.important ? 'important' : ''}</strong>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
