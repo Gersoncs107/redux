77661fbfca99d6384b38b77592471f28197eba66
Gersoncs107 <gerson.silva107@gmail.com>
2026-02-03 22:14:43 -0300

    redux-update

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client'
-import { createStore,combineReducers } from 'redux'
 import { Provider } from 'react-redux'
+import { createStore, combineReducers } from 'redux'
 
 import App from './App'
 import filterReducer from './reducers/filterReducer'
@@ -14,13 +14,6 @@ const reducer = combineReducers({
 const store = createStore(reducer)
 
 console.log(store.getState())
-
-import { createNote } from './reducers/noteReducer'
-import { filterChange } from './reducers/filterReducer'
-
-store.subscribe(() => console.log(store.getState()))
-store.dispatch(filterChange('IMPORTANT'))
-store.dispatch(createNote('combineReducers forms one reducer from many simple reducers'))
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
